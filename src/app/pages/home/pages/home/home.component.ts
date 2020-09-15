9378b07adc0364b9c3f23200ac764734e18251bd
mrblack360 <rmaswi360@outlook.com>
2020-09-15 15:43:59 +0300

    refactor: modified the structure of home module

--- src/app/pages/home/pages/home/home.component.ts
+++ src/app/pages/home/pages/home/home.component.ts
@@ -6,16 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  sections = [
-    { name: 'SECTION 1', link: '' },
-    { name: 'SECTION 2', link: '' },
-    { name: 'SECTION 3', link: '' },
-    { name: 'SECTION 4', link: '' },
-    { name: 'SECTION 5', link: '' },
-    { name: 'SECTION 6', link: '' },
-    { name: 'SECTION 7', link: '' },
-    { name: 'SECTION 8', link: '' },
-  ];
   constructor() {}
 
   ngOnInit() {}
