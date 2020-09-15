d521345ed07931e5d63ce5fbd0ec76db02f75696
mrblack360 <rmaswi360@outlook.com>
2020-09-15 15:42:17 +0300

    Created instructor context module

---
+++ src/app/pages/instructor/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+  sections = [
+    { name: 'SECTION 1', link: '' },
+    { name: 'SECTION 2', link: '' },
+    { name: 'SECTION 3', link: '' },
+    { name: 'SECTION 4', link: '' },
+    { name: 'SECTION 5', link: '' },
+    { name: 'SECTION 6', link: '' },
+    { name: 'SECTION 7', link: '' },
+    { name: 'SECTION 8', link: '' },
+  ];
+  constructor() {}
+
+  ngOnInit(): void {}
+}
