3fb00521ddadc1313007c83fd9a1abe4b0dab003
mrblack360 <rmaswi360@outlook.com>
2020-09-24 10:00:10 +0300

    feat: Added the side bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,13 @@ import { Title } from '@angular/platform-browser';
 import { Fn } from '@iapps/function-analytics';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { FlatTreeControl } from '@angular/cdk/tree';
+import {
+  MatTreeFlatDataSource,
+  MatTreeFlattener,
+} from '@angular/material/tree';
 
 @Component({
   selector: 'app-root',
@@ -11,21 +18,59 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  contexts = [
-    { id: 'instructor', name: 'Instructor' },
-    { id: 'hod', name: 'Head of Department' },
+  panelOpenState = false;
+  sections = [
+    { name: 'Dashboard', link: 'dashboard' },
+    { name: 'Planning', link: 'planning' },
+    {
+      name: 'Objectives Agreement',
+      link: 'objectives-agreement',
+      children: [
+        { name: 'My Objectives', link: 'objectives-agreement/my-objectives' },
+        {
+          name: 'Staff Objectives',
+          link: 'objectives-agreement/staff-objectives',
+        },
+      ],
+    },
+    {
+      name: 'Performance Review',
+      link: 'performance-review',
+      children: [
+        { name: 'Self Review', link: 'performance-review/self-review' },
+        { name: 'Staff Review', link: 'performance-review/staff-review' },
+      ],
+    },
   ];
-  periods = [
-    { id: 'wwse', name: '22019/2020' },
-    { id: 'gshs', name: '2020/2021' },
-  ];
-  selectedContext = this.contexts[0].id;
-  selectedPeriod = this.periods[1].id;
+  private _transformer = (node: any, level: number) => {
+    return {
+      expandable: !!node.children && node.children.length > 0,
+      name: node.name,
+      link: node.link,
+      level: level,
+    };
+  };
+
+  treeControl = new FlatTreeControl<any>(
+    (node) => node.level,
+    (node) => node.expandable
+  );
+
+  treeFlattener = new MatTreeFlattener(
+    this._transformer,
+    (node) => node.level,
+    (node) => node.expandable,
+    (node) => node.children
+  );
+
+  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
   constructor(
     private translate: TranslateService,
     private titleService: Title,
     private router: Router,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    public dialog: MatDialog
   ) {
     // this language will be used as a fallback when a translation isn't found in the current language
     this.translate.setDefaultLang('en');
@@ -41,22 +86,14 @@ export class AppComponent implements OnInit {
         baseUrl: '../../../',
       });
     }
+    // Set tree dataSources
+    this.dataSource.data = this.sections;
   }
-  ngOnInit(): void {
-    this.selectContext();
-  }
+  ngOnInit(): void {}
+
+  hasChild = (_: number, node: any) => node.expandable;
 
   public setTitle(newTitle: string) {
     this.titleService.setTitle(newTitle);
-  }
-  selectContext() {
-    console.log(this.selectedContext);
-
-    this.router.navigate([this.selectedContext]);
-  }
-  selectPeriod() {
-    this.snackBar.open('UNDER CONSTRUCTION, COMING SOON...', 'OK', {
-      duration: 3000,
-    });
   }
 }
