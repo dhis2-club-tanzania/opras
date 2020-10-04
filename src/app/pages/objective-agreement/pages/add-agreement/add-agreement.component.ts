b570fa62489d9a75ada4fdaeb2f487b5033f286b
mrblack360 <rmaswi360@outlook.com>
2020-10-04 17:47:16 +0300

    feat: created the add agreement component

---
+++ src/app/pages/objective-agreement/pages/add-agreement/add-agreement.component.ts
@@ -0,0 +1,25 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-add-agreement',
+  templateUrl: './add-agreement.component.html',
+  styleUrls: ['./add-agreement.component.css']
+})
+export class AddAgreementComponent implements OnInit {
+  canAddAgreement: Boolean = false;
+  agreement={target:'',criteria:'',resource:''};
+
+  constructor(public dialogRef: MatDialogRef<AddAgreementComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  ngOnInit(): void {
+  }
+  onCancelClick(): void {
+    this.dialogRef.close();
+  }
+  addAgreementToggle() {
+    if (this.agreement.target.length == 0 || this.agreement.criteria.length == 0 || this.agreement.resource.length == 0) { } else this.canAddAgreement = true;
+  }
+
+}
