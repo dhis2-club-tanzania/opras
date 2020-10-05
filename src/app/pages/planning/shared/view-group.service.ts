80a334711cd3d11616edd1a095b2375861ae2925
mrblack360 <rmaswi360@outlook.com>
2020-10-05 18:58:53 +0300

    feat(planning): modified the routes for the module

--- src/app/pages/planning/shared/view-group.service.ts
+++ src/app/pages/planning/shared/view-group.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { of, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ViewGroupService {
-
-  constructor() { }
+  private currentGroup: Observable<any>;
+  constructor() {
+    this.currentGroup = of('ts');
+  }
+  public getCurrentGroup(): Observable<string> {
+    this.currentGroup.subscribe(
+      (data) => {
+        return data;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+    return this.currentGroup;
+  }
+  public setGroup(index: number): void {
+    if (index == 1) {
+      this.currentGroup = of('as');
+    } else {
+      this.currentGroup = of('ts');
+    }
+  }
 }
