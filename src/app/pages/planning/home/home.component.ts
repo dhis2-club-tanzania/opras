c9fff13adf4ca07b75309ffbf66d46ffeff1e443
mrblack360 <rmaswi360@outlook.com>
2020-10-06 18:07:16 +0300

    feat: created a home component for planning module

--- src/app/pages/planning/home/home.component.ts
+++ src/app/pages/planning/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -12,29 +10,12 @@ export class HomeComponent implements OnInit {
     { id: 'dffgdd', name: 'teaching staff' },
     { id: 'sauifdj', name: 'Administrative staff' },
   ];
-  constructor(private snackBar: MatSnackBar, private router: Router) {}
+  groupState = 0;
+  constructor() {}
 
   ngOnInit(): void {}
 
-  createNewGroup() {
-    this.snackBar.open('This feature will be implemented soon', 'OK', {
-      duration: 3000,
-    });
-  }
-  viewGroup(index: number) {
-    this.router.navigate(['planning/view-group']);
-  }
-  editGroup() {
-    this.snackBar.open("We'll edit the group", 'OK', { duration: 3000 });
-  }
-  deleteGroup(index: number) {
-    this.snackBar.open(
-      "Nex time, we'll delete " + this.groups[index].name + ' permanently',
-      'OK',
-      { duration: 4000 }
-    );
-  }
   setGroupState(event) {
-    console.log(event);
+    this.groupState = event;
   }
 }
