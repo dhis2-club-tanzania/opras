b2cae4871f81b0525f63fd9a5361bde7c17479c0
mrblack360 <rmaswi360@outlook.com>
2020-10-09 18:44:34 +0300

    feat(objective-agreement): created the routes for the child components

--- src/app/pages/objective-agreement/objective-agreement.module.ts
+++ src/app/pages/objective-agreement/objective-agreement.module.ts
@@ -1,16 +1,50 @@
+// Core imports
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+// Components imports
 import { HomeComponent } from './home/home.component';
 import { MyObjectivesComponent } from './pages/my-objectives/my-objectives.component';
 import { StaffObjectivesComponent } from './pages/staff-objectives/staff-objectives.component';
 import { ObjectiveAgreementRoutingModule } from './objective-agreement-routing.module';
+import { AddAgreementComponent } from './pages/add-agreement/add-agreement.component';
+
+// Material imports
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     HomeComponent,
     MyObjectivesComponent,
     StaffObjectivesComponent,
+    AddAgreementComponent,
   ],
-  imports: [CommonModule, ObjectiveAgreementRoutingModule],
+  imports: [
+    CommonModule,
+    ObjectiveAgreementRoutingModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatExpansionModule,
+    MatDialogModule,
+    MatCardModule,
+    MatInputModule,
+    FormsModule,
+  ],
 })
 export class ObjectiveAgreementModule {}
