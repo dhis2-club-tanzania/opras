dcb59fa92e7dc1f66826a446c79280fab7eea5a7
mrblack360 <rmaswi360@outlook.com>
2020-10-19 17:21:48 +0300

    refactor(review): added the missing material modules

--- src/app/pages/performance-review/performance-review.module.ts
+++ src/app/pages/performance-review/performance-review.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { PerformanceReviewRoutingModule } from './performance-review-routing.module';
 import { SelfReviewComponent } from './pages/self-review/self-review.component';
 import { StaffReviewComponent } from './pages/staff-review/staff-review.component';
 import { MidYearReviewComponent } from './pages/mid-year-review/mid-year-review.component';
+import { AddReviewComponent } from './pages/add-review/add-review.component';
+import { AnnualReviewComponent } from './pages/annual-review/annual-review.component';
 
 // Material Imports
 import { MatTabsModule } from '@angular/material/tabs';
@@ -11,7 +15,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
-import { AddReviewComponent } from './pages/add-review/add-review.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
@@ -19,6 +27,7 @@ import { AddReviewComponent } from './pages/add-review/add-review.component';
     StaffReviewComponent,
     MidYearReviewComponent,
     AddReviewComponent,
+    AnnualReviewComponent,
   ],
   imports: [
     CommonModule,
@@ -28,6 +37,13 @@ import { AddReviewComponent } from './pages/add-review/add-review.component';
     MatExpansionModule,
     MatButtonModule,
     MatTableModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatSliderModule,
   ],
 })
 export class PerformanceReviewModule {}
