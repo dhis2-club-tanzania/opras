56b5eb0daf57a77c881c2df3196cd221700e6bd6
mrblack360 <rmaswi360@outlook.com>
2020-10-19 17:19:54 +0300

    feat(review): modified the add review component

--- src/app/pages/performance-review/pages/add-review/add-review.component.ts
+++ src/app/pages/performance-review/pages/add-review/add-review.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-add-review',
   templateUrl: './add-review.component.html',
-  styleUrls: ['./add-review.component.css']
+  styleUrls: ['./add-review.component.css'],
 })
 export class AddReviewComponent implements OnInit {
+  canReview: Boolean = false;
+  review = { ptt: '', fap: '' };
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<AddReviewComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onCancelClick(): void {
+    this.dialogRef.close('cancel');
   }
-
+  addReviewToggle() {
+    if (this.review.ptt.length == 0 || this.review.fap.length == 0) {
+    } else this.canReview = true;
+  }
 }
