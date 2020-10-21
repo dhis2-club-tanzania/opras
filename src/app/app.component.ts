a89df5384ec85eb0613ab6c2375d5d090299f00e
mrblack360 <rmaswi360@outlook.com>
2020-10-21 09:10:27 +0300

    feat(app): shifted the sidebar from app to home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,8 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { Title } from '@angular/platform-browser';
 import { Fn } from '@iapps/function-analytics';
-import { Router } from '@angular/router';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatDialog } from '@angular/material/dialog';
-import { FlatTreeControl } from '@angular/cdk/tree';
-import {
-  MatTreeFlatDataSource,
-  MatTreeFlattener,
-} from '@angular/material/tree';
+
 import { MatMenuTrigger } from '@angular/material/menu';
 
 @Component({
@@ -19,58 +13,10 @@ import { MatMenuTrigger } from '@angular/material/menu';
 })
 export class AppComponent implements OnInit {
   @ViewChild(MatMenuTrigger) trigger: MatMenuTrigger;
-  panelOpenState = false;
-  sections = [
-    { name: 'Dashboard', link: 'dashboard' },
-    { name: 'Planning', link: 'planning' },
-    {
-      name: 'Objectives Agreement',
-      link: 'objectives-agreement',
-      children: [
-        { name: 'My Objectives', link: 'objectives-agreement/my-objectives' },
-        {
-          name: 'Staff Objectives',
-          link: 'objectives-agreement/staff-objectives',
-        },
-      ],
-    },
-    {
-      name: 'Performance Review',
-      link: 'performance-review',
-      children: [
-        { name: 'Self Review', link: 'performance-review/self-review' },
-        { name: 'Staff Review', link: 'performance-review/staff-review' },
-      ],
-    },
-  ];
-  private _transformer = (node: any, level: number) => {
-    return {
-      expandable: !!node.children && node.children.length > 0,
-      name: node.name,
-      link: node.link,
-      level: level,
-    };
-  };
-
-  treeControl = new FlatTreeControl<any>(
-    (node) => node.level,
-    (node) => node.expandable
-  );
-
-  treeFlattener = new MatTreeFlattener(
-    this._transformer,
-    (node) => node.level,
-    (node) => node.expandable,
-    (node) => node.children
-  );
-
-  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
 
   constructor(
     private translate: TranslateService,
     private titleService: Title,
-    private router: Router,
-    private snackBar: MatSnackBar,
     public dialog: MatDialog
   ) {
     // this language will be used as a fallback when a translation isn't found in the current language
@@ -87,12 +33,8 @@ export class AppComponent implements OnInit {
         baseUrl: '../../../',
       });
     }
-    // Set tree dataSources
-    this.dataSource.data = this.sections;
   }
   ngOnInit(): void {}
-
-  hasChild = (_: number, node: any) => node.expandable;
 
   public setTitle(newTitle: string) {
     this.titleService.setTitle(newTitle);
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,69 @@
+import { FlatTreeControl } from '@angular/cdk/tree';
 import { Component, OnInit } from '@angular/core';
+import {
+  MatTreeFlatDataSource,
+  MatTreeFlattener,
+} from '@angular/material/tree';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  panelOpenState = false;
+  sections = [
+    { name: 'Dashboard', link: '/dashboard' },
+    { name: 'Planning', link: '/planning' },
+    {
+      name: 'Objectives Agreement',
+      link: '/objectives-agreement',
+      children: [
+        { name: 'My Objectives', link: '/objectives-agreement/my-objectives' },
+        {
+          name: 'Staff Objectives',
+          link: '/objectives-agreement/staff-objectives',
+        },
+      ],
+    },
+    {
+      name: 'Performance Review',
+      link: '/performance-review',
+      children: [
+        { name: 'Self Review', link: '/performance-review/self-review' },
+        { name: 'Staff Review', link: '/performance-review/staff-review' },
+      ],
+    },
+  ];
+  private _transformer = (node: any, level: number) => {
+    return {
+      expandable: !!node.children && node.children.length > 0,
+      name: node.name,
+      link: node.link,
+      level: level,
+    };
+  };
 
-  constructor() { }
+  treeControl = new FlatTreeControl<any>(
+    (node) => node.level,
+    (node) => node.expandable
+  );
 
-  ngOnInit(): void {
+  treeFlattener = new MatTreeFlattener(
+    this._transformer,
+    (node) => node.level,
+    (node) => node.expandable,
+    (node) => node.children
+  );
+
+  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
+  constructor() {
+    // Set tree dataSources
+    this.dataSource.data = this.sections;
   }
 
+  ngOnInit(): void {}
+
+  hasChild = (_: number, node: any) => node.expandable;
 }
