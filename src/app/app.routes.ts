f33d06c26aefe557c7e2666f6673245f88873feb
mrblack360 <rmaswi360@outlook.com>
2020-10-21 09:17:12 +0300

    feat(app): modified the app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,34 +1,43 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   {
-    path: 'dashboard',
-    loadChildren: () =>
-      import('./pages/dashboard/dashboard.module').then(
-        (m) => m.DashboardModule
-      ),
-  },
-  {
-    path: 'planning',
-    loadChildren: () =>
-      import('./pages/planning/planning.module').then((m) => m.PlanningModule),
-  },
-  {
-    path: 'objectives-agreement',
-    loadChildren: () =>
-      import('./pages/objective-agreement/objective-agreement.module').then(
-        (m) => m.ObjectiveAgreementModule
-      ),
-  },
-  {
-    path: 'performance-review',
-    loadChildren: () =>
-      import('./pages/performance-review/performance-review.module').then(
-        (m) => m.PerformanceReviewModule
-      ),
+    path: '',
+    component: HomeComponent,
+    children: [
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+      {
+        path: 'dashboard',
+        loadChildren: () =>
+          import('./pages/dashboard/dashboard.module').then(
+            (m) => m.DashboardModule
+          ),
+      },
+      {
+        path: 'planning',
+        loadChildren: () =>
+          import('./pages/planning/planning.module').then(
+            (m) => m.PlanningModule
+          ),
+      },
+      {
+        path: 'objectives-agreement',
+        loadChildren: () =>
+          import('./pages/objective-agreement/objective-agreement.module').then(
+            (m) => m.ObjectiveAgreementModule
+          ),
+      },
+      {
+        path: 'performance-review',
+        loadChildren: () =>
+          import('./pages/performance-review/performance-review.module').then(
+            (m) => m.PerformanceReviewModule
+          ),
+      },
+    ],
   },
   {
     path: 'settings',
