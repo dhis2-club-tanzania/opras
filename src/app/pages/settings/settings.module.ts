8678ff130fe3bb461cc90fe848c343a578bd7801
mrblack360 <rmaswi360@outlook.com>
2020-10-21 18:56:55 +0300

    feat(settings): added require material modules

--- src/app/pages/settings/settings.module.ts
+++ src/app/pages/settings/settings.module.ts
@@ -1,12 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+// Material imports
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+
+// Components
 import { HomeComponent } from './home/home.component';
 import { SettingsRoutingModule } from './settings-routing.module';
 import { GeneralComponent } from './pages/general/general.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
-  declarations: [HomeComponent, GeneralComponent, ProfileComponent],
-  imports: [CommonModule, SettingsRoutingModule],
+  declarations: [
+    HomeComponent,
+    GeneralComponent,
+    ProfileComponent,
+    AboutComponent,
+  ],
+  imports: [
+    CommonModule,
+    SettingsRoutingModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule,
+  ],
 })
-export class SettingsModule { }
+export class SettingsModule {}
