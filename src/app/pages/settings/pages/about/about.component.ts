1739056c46c7106a1429e27ce83007a79ccb35fc
mrblack360 <rmaswi360@outlook.com>
2020-10-27 03:28:04 +0300

    feat(settings): added the about description

--- src/app/pages/settings/pages/about/about.component.ts
+++ src/app/pages/settings/pages/about/about.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { version } from '../../../../../../package.json';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
+  public version: string = version;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
